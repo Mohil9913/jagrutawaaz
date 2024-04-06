15b68e6dcca9d6a5e838a61ce19e4902aca1fc06
Mohil Mokaria <67378174+Mohil9913@users.noreply.github.com>
2024-04-06 16:16:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const netAmountInput = document.getElementById("net-amount");
+const platformFeesInput = document.getElementById("platform-fees");
+const personalCutInput = document.getElementById("personal-cut");
+const result = document.getElementById("result");
+const resultText = document.getElementById("result-text");
+
+resultText.style.display = "none";
+
+const calculateAukat = () => {
+    const netAmount = parseFloat(netAmountInput.value);
+    const platformFees = parseFloat(platformFeesInput.value);
+    const personalCut = parseFloat(personalCutInput.value);
+
+    if (isNaN(netAmount) || isNaN(platformFees) || isNaN(personalCut)) {
+        alert("Invalid input. Please enter valid numbers.");
+        return;
+    }
+
+    const platformPercentage = platformFees / 100;
+    const personalPercentage = personalCut / 100;
+    
+    let total = netAmount - (netAmount * platformPercentage); 
+    total = (total * personalPercentage); 
+
+    resultText.style.display = "block";
+    result.textContent = `₹${total}`;
+}
